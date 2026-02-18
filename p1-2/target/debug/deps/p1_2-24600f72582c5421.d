b21b15619d912df5e799ef9ae79c506cb98ff494
ao81 <mc4@outlook.jp>
2026-02-18 22:21:43 +0900

    p1-2を作成

---
+++ p1-2/target/debug/deps/p1_2-24600f72582c5421.d
@@ -0,0 +1,5 @@
+/home/ao/rust/p1-2/target/debug/deps/p1_2-24600f72582c5421.d: src/main.rs
+
+/home/ao/rust/p1-2/target/debug/deps/libp1_2-24600f72582c5421.rmeta: src/main.rs
+
+src/main.rs:
---
+++ p1-2/target/debug/p1-2.d
@@ -0,0 +1,1 @@
+/home/ao/rust/p1-2/target/debug/p1-2: /home/ao/rust/p1-2/src/main.rs
